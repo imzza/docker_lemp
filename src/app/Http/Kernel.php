6207dad4a35fa9d8377cf2d705176a7b508eeebe
unknown <admin@TLDZ-L-73.Techleadz.Local>
2020-08-24 20:28:43 +0500

    Added Laravel telescop

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -4,6 +4,11 @@ namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
+
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+
+
+
 class Kernel extends HttpKernel
 {
     /**
@@ -40,6 +45,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+             EnsureFrontendRequestsAreStateful::class,
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
