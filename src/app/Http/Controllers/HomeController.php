31f613eb304932bb4b57be755e6ef29d3e8f0607
unknown <admin@TLDZ-L-73.Techleadz.Local>
2020-08-27 17:30:54 +0500

    Added the token Route

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Http;
 
 class HomeController extends Controller
 {
@@ -24,5 +25,23 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function token(Request $request) {
+
+        return response()->json($request->all());
+
+        $http = new GuzzleHttp\Client;
+        $response = $http->post('https://localhost/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'password',
+                'client_id' => '916298a1-4ade-465d-abe8-5e1e6edaa288',
+                'client_secret' => 'IGhiIMIsi7LiTH1k9LSruCvj3YJloVMRkeWX58CW',
+                'username' => $request->username,
+                'password' => $request->password,
+                'scope' => '',
+            ],
+        ]);
+        return json_decode((string) $response->getBody(), true);
     }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -18,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
