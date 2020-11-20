fe12ae9cf6c20b52d71dc82fe075888f22c7cb74
IRFAN ULLAH <irfanullah73@gmail.com>
2020-11-20 12:46:14 +0500

    Status Files

--- flask/app.py
+++ flask/app.py
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import time
-
-import redis
-from flask import Flask
-
-app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
-
-
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
-
-
-@app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
-
-@app.route('/flask')
-def flask_route():
-    count = get_hit_count()
-=======
 import time
 
 import redis
@@ -57,5 +26,4 @@ def hello():
 @app.route('/flask')
 def flask_route():
     count = get_hit_count()
->>>>>>> 3d8c0fc3b8b01d625f91237c844a507df18ac675
     return 'Hello World! Flask Route {} times.\n'.format(count)
